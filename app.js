618e622e4d703dc2e339d8cc85c2e1e275d20be8
juanruizl <160032371+juanruizl@users.noreply.github.com>
2024-07-22 09:29:39 +0000

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-// Listas de pronombres, adjetivos y sustantivos
 let pronoun = ['the', 'our', 'my', 'your'];
 let adj = ['great', 'big', 'fast', 'smart'];
 let noun = ['jogger', 'racoon', 'developer', 'car'];
 let extensions = ['.com', '.net', '.us', '.io'];
 
-// Generar combinaciones de nombres de dominio
 for (let p of pronoun) {
   for (let a of adj) {
     for (let n of noun) {
@@ -15,13 +13,11 @@ for (let p of pronoun) {
   }
 }
 
-// Generar combinaciones con domain hacks
-let domainHacks = ['es']; // Puedes agregar más hacks de dominio aquí
+let domainHacks = ['es']; 
 for (let p of pronoun) {
   for (let a of adj) {
     for (let n of noun) {
       for (let hack of domainHacks) {
-        // Verificar si el sustantivo termina con las letras de la extensión hack
         if (n.endsWith(hack)) {
           let hackDomain = n.slice(0, -hack.length) + '.' + hack;
           console.log(p + a + hackDomain);
